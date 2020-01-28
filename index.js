e1a276ee975b0d45785aa9b4be1fcf42e5d8de5b
saejung <sss804@naver.com>
2020-01-28 17:34:13 +0900

    3 commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hi Wetube3!");
+import express from "express";
+//const express = require('express');
+const app = express();
+const PORT = 4000;
+
+const handleListening = () =>
+    console.log(`Listening on : http://localhost:${PORT}`);
+
+
+const handleHome = (req, res) => res.send('Hello from home!!');
+
+
+const handleProfile = (req, res) => res.send("You are on my sjk");
+
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
