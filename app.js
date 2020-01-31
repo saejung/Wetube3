f68975357201446f9e1e9de6226683b89140fc98
saejung <sss804@naver.com>
2020-01-31 18:03:56 +0900

    view pug commit

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routers from "./routes";
 import userRouter from "./routers/userRouter"; 
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routers from "./routes";
 
 const app = express();
 
-app.use(cookieParser());
-
-//서버가 유저로 부터 받은 데이터를 이해하는 방법
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended: true})); 
 app.use(helmet());
+app.set('view engine', "pug");
+app.use(cookieParser());
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: true})); //서버가 유저로 부터 받은 데이터를 이해하는 방법
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use(routers.home, globalRouter);
 app.use(routers.users, userRouter);
