2c6eb92c97438ba5ffce17dfe32736b76ef951d7
saejung <sss804@naver.com>
2020-01-31 11:35:27 +0900

    #2.11 MVC Pattern part Three Commit

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,13 @@
 import express from "express";
+import routes from "../routes";
+import { videos, upload, videoDetail, editVideo, detailVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.detailVideo, detailVideo);
 
 export default videoRouter;
